2c927ecadf79f992cf58895abf119f0ef57eeb6c
Vitalii Matviichuk <matv.vitalii@gmail.com>
2023-12-05 20:02:23 +0100

    Recommended changes have been made.

--- src/main/java/FileService/FileService.java
+++ src/main/java/FileService/FileService.java
@@ -13,11 +13,10 @@ import java.util.ArrayList;
 
 public class FileService {
 
-    public ArrayList<Character> FileRead(String command) throws IOException {
+    public ArrayList<Character> fileRead(String command) throws IOException {
         CLI cli = new CLI();
-        String path= cli.getPath();
-        StringBuffer fileData = new StringBuffer();
-        BufferedReader reader = new BufferedReader(new FileReader(path));
+        command = cli.getPath();
+        BufferedReader reader = new BufferedReader(new FileReader(command));
         ArrayList<Character> textOfInput = new ArrayList<>();
         char[] buf = new char[1024];
         int numRead = reader.read(buf);
@@ -29,11 +28,11 @@ public class FileService {
         return textOfInput;
     }
 
-    public void FileWrite(String command, String path) throws IOException {
+    public void fileWrite(String command) throws IOException {
         CLI cli = new CLI();
         CaesarCipher caesarCipher = new CaesarCipher();
         Path fileOut = Files.createFile(Path.of(cli.getPath() + "[" + command + "]"));
-        Files.writeString(fileOut, caesarCipher.ChoiceOfCommand(command), StandardOpenOption.APPEND);
+        Files.writeString(fileOut, caesarCipher.choiceOfCommand(command), StandardOpenOption.APPEND);
     }
 }
 
